7e5dfd1e293488cdf49cfa9c2cf3ad2a7162bb57
spxxt02 <66674172+spxxt02@users.noreply.github.com>
2025-08-19 23:45:09 +0800

    Update commit-script.sh

--- .github/commit-script.sh
+++ .github/commit-script.sh
@@ -6,7 +6,8 @@ cd output
 git init
 git config --local user.email "github-action@users.noreply.github.com"
 git config --local user.name "GitHub Action"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/lhear/geosite.git
+#git remote add origin https://github-action:$GITHUB_TOKEN@github.com/lhear/geosite.git
+git remote add origin https://github-action:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
 git branch -M release
 git add .
 git commit -m "Update release"
